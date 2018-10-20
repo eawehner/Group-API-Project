1e385c5848884d03fff8bbea7a6f61ab8d4a3b10
EAWehner <elizabethawehner@gmail.com>
2018-10-20 10:20:49 -0400

    fixed hash issue, can successfully make API calls to Marvel API

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script>
-
-
     var public_api = "0fe98b910165d9ca2dc0abf1bc48fca1";
-    var private_api = "1f9d9779591c0d481b10e7d48b547b5c50e7935";
+    var private_api = "71f9d9779591c0d481b10e7d48b547b5c50e7935";
     var ts = Math.floor(Math.random() * Math.floor(1000));
     var hash = CryptoJS.MD5(`${ts}${private_api}${public_api}`);
     var queryURL = `http://gateway.marvel.com/v1/public/comics?apikey=${public_api}&hash=${hash}&ts=${ts}`
