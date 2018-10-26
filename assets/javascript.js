f27d4f641471c85a26b2f86ff8bbac37fa47a85c
EAWehner <elizabethawehner@gmail.com>
2018-10-25 21:09:26 -0400

    the end is nigh

--- assets/javascript.js
+++ assets/javascript.js
@@ -13,7 +13,7 @@ function aMarvel() {
   var private_api = "71f9d9779591c0d481b10e7d48b547b5c50e7935";
   var ts = Math.floor(Math.random() * Math.floor(1000));
   var hash = CryptoJS.MD5(`${ts}${private_api}${public_api}`);
-  var queryURL = `http://gateway.marvel.com/v1/public/characters?limit=20&offset=${offset}&apikey=${public_api}&hash=${hash}&ts=${ts}`
+  var queryURL = `https://gateway.marvel.com/v1/public/characters?limit=20&offset=${offset}&apikey=${public_api}&hash=${hash}&ts=${ts}`
 
   $.ajax({
       url: queryURL,
